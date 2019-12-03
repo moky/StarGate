51212b2e59205940684f03a019a3d2ad5542df3b
John Chen <moonfunjohn@gmail.com>
2019-12-03 16:43:40 +0800

    header file

---
+++ MarsGate/mars/mars/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION ""
+#define MARS_PATH ""
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2019-10-12 21:12:50"
+#define MARS_TAG ""
+
+#endif
